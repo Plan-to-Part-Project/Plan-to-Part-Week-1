7e9db598cdc7b7625e82f309634377008eb8041b
Michael Drozdowski <mjd76@njit.edu>
2021-02-23 21:06:08 -0500

    update register

--- register_user.php
+++ register_user.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'./vendor/autoload.php';
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 use Kreait\Firebase\Auth;
@@ -9,7 +9,7 @@ if(isset($_POST['push'])){
     $email = $_POST['email'];
     $pass = $_POST['pass'];
 
-    $factory = (new Factory)->withServiceAccount('./secret/plantopart-4c826-firebase-adminsdk-quxvu-242e63036c.json')->createAuth()->createUserWithEmailAndPassword($email, $pass);
+    $factory = (new Factory)->withServiceAccount('./secret/plantopart-4c826-firebase-adminsdk-quxvu-6de9295712.json')->createAuth()->createUserWithEmailAndPassword($email, $pass);
     header("Location: homePage.html");
 }
 
