f3cb2fe00e3e6d741cdf76d5a799b4b3d692db77
henil14 <henil1008@gmail.com>
2021-03-01 10:59:00 -0500

    digital working

--- register_user.php
+++ register_user.php
@@ -8,31 +8,30 @@ use Kreait\Firebase\Auth;
 if(isset($_POST['push'])){
     $email = $_POST['email'];
     $pass = $_POST['pass'];
-
-    $factory = (new Factory)->withServiceAccount('./secret/plantopart-4c826-firebase-adminsdk-quxvu-242e63036c.json')->createAuth()->createUserWithEmailAndPassword($email, $pass);
-    header("Location: homePage.html");
-}
-
-
-/*
-if(isset($_POST['push'])) {
+    //creates an new user with email and pass.
+    $u = (new Factory)->withServiceAccount('./secret/plantopart-4c826-firebase-adminsdk-quxvu-242e63036c.json')->createAuth()->createUserWithEmailAndPassword($email, $pass)->uid;
+    $factory = (new Factory)->withServiceAccount('./secret/plantopart-4c826-firebase-adminsdk-quxvu-242e63036c.json');
+    $temp = $factory->createDatabase();
     $name = $_POST['name'];
-    $email = $_POST['email'];
-
     $data = [
         'name' => $name,
-        'email' => $email
+        'email' => $email,
+        ];
+
+    $newPostKey = $temp->getReference('Users')->push()->getKey();
+
+    $parent_data = [
+        //'Users/'.$newPostKey => $data,
+        'Users/'.$u."/User_Data" => $data,
     ];
 
 
-    $factory = (new Factory)->withServiceAccount('./secret/plantopart-4c826-firebase-adminsdk-quxvu-242e63036c.json');
-    $temp = $factory->createDatabase();
 //die(print_r($data));
 
-    $temp->getReference('Users/')->push($data);
+    $temp->getReference()->update($parent_data);
 
-
-    header("Location: homePage.html");
+    //echo $_POST['email'];
+    header("Location: ./formPage/Introduction.html");
+    //echo "Done";
 }
 ?>
-*/
