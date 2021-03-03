d4417852844f4d7f1a8597427e8067055f779ae1
Michael Drozdowski <mjd76@njit.edu>
2021-03-03 17:02:02 -0500

    Temporary login.php file

---
+++ loginPage/login.php
@@ -0,0 +1,20 @@
+<?php
+require "C:\Users\Michael\PhpstormProjects\Plan-to-Part-Week-1/vendor\autoload.php";
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Auth;
+
+//This file is used to check if the user's email is marked as verified
+$email = $_POST ['inputEmail'];
+$pass = $_POST['inputPassword'];
+
+$factory = (new Factory)->withServiceAccount('C:\Users\Michael\PhpstormProjects\Plan-to-Part-Week-1\secret\plantopart-4c826-firebase-adminsdk-quxvu-242e63036c.json');
+//$u = $factory->createAuth()->getUserByEmail($email)->uid;
+$verified = $factory->createAuth()->getUserByEmail($email)->emailVerified;
+
+if ($verified == true) {
+    header("Location: ../Dashboard_SideBar/Membership_Section/membership.html");
+}elseif ($verified==false){
+    header("Location: login.html");
+}
