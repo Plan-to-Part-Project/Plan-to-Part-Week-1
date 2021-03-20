5daf0a5354478b68dd8a1b04fbcaf3e430e95be0
Zaafira Hasan <zfh4@njit.edu>
2021-03-19 23:06:33 -0400

    started health.html and socialmedia.html

--- Dashboard_Sidebar/Financial_Personal_Sections/Financial_load.php
+++ Dashboard_Sidebar/Financial_Personal_Sections/Financial_load.php
@@ -1,5 +1,5 @@
 <?php
-require 'C:\Users\henil\Desktop\P2P\Plan-to-Part-Week-1\vendor\autoload.php';
+require __DIR__.'/../../vendor/autoload.php';
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 use Kreait\Firebase\Auth;
---
+++ Dashboard_Sidebar/Health_Section/health.php
@@ -0,0 +1,30 @@
+<?php
+require __DIR__."/../../vendor/autoload.php";
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Auth;
+
+$response = file_get_contents('php://input');
+$res = json_decode($response, true);
+if(isset($res)){
+    $factory = (new Factory)->withServiceAccount('../../secret/plantopart-4c826-firebase-adminsdk-quxvu-242e63036c.json');
+    $user = $factory->createAuth()->getUserByEmail($res['email'])->uid;
+    $newPostKey = $factory->createDatabase()->getReference('Users')->push()->getKey();
+    $data = [];
+    foreach ($res as $key => $value) {
+        if(strcmp($key,"")) {
+            if (strcmp($key, "email")) {
+                if (isset($res[$key])) {
+                    $data += ["$key" => $value];
+                }
+            }
+        }
+    }
+    $parent_data = [
+        //'Users/'.$newPostKey => $data,
+        'Users/'.$user."/Health_Data" => $data,
+    ];
+    $factory->createDatabase()->getReference()->update($parent_data);
+    echo json_encode('done');
+}
+?>
